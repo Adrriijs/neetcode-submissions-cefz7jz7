a56b148c175506ca84333224a7cab736785cabde
ADRIAN <109157046+Adrriijs@users.noreply.github.com>
2026-08-28 16:04:38 +0700

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,46 @@
+class Solution:
+    """
+    simple encode and decode
+    n#word n for the length, # just seperator
+
+    hello, world = 5#hello5#world
+    """
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+
+        return res
+
+    """
+    convert number first i to j
+    """
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            
+            length = int(s[i:j])
+            start = j + 1
+            end = start + length
+            res.append(s[start:end])
+            i = end
+
+            """
+            5#hello
+
+            length = 5
+            start = 2
+            end = 7
+            : end until index 6 (end doesnt count)
+
+            i = 7
+            """
+        
+        return res
